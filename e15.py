1d1077846363570ebae131633f972b87ade4c26f
me@wngz3r0.info <me@wngz3r0.info>
2012-03-31 00:03:06 -0700

    fixing comments

--- e15.py
+++ e15.py
@@ -2,7 +2,7 @@ def arr_print(rows):
     print ("\n".join(["\t".join(map(str, row)) for row in rows]))
     return 0
 
-n = 20
+n = 5
 rows = [[0] * n for _ in range(n)]
 # This was my incorrect first attempt: rows = [[[0] * n] * n]. 
 # The outer lists are just a reference to the same inner list!
@@ -12,10 +12,11 @@ for i in range(len(rows)):
         # This is a bit of a hack. It uses the fact that 
         # lst[-n] = lst[-n + len(lst)]. lst[-1] == lst[len(-1)]
         # Therefore, when accessing the items on the edge of a row or column,
-        # it actually accesses the last array entry (i.e. i or j == 0). Since
-        # this is initialised to 0, and the for loop is increasing, 
-        # this evaluates to 0, and the max function sets it to 1
+        # (i.e. when i or j == 0) it actually accesses the last array entry.
+        # For example, (i,j) == (0, 1) accesses rows[n-1][1] and rows[0,0]
+        # Rows is initialized to 0, and therefore this defaults to 0
         rows[i][j] = max(1, rows[i - 1][j] + rows[i][j - 1])
 
 arr_print(rows)
 print (rows[-1][-1])
+
